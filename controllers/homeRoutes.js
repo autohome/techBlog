8a00739504349597062be0ccb88fb8bf6a552502
autohome <joshdadbin@gmail.com>
2023-05-01 16:47:53 -0700

    dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,14 +41,20 @@ router.get('/post/:id', async (req, res) => {
         },
         {
           model: Comment,
-          attributes: ['contents'],
+          attributes: ['contents', 'date_created'],
+          include: [
+            {
+              model: User,
+              attributes: ['name'],
+            },
+          ],
         },
       ],
     });
 
     const post = postData.get({ plain: true });
 
-    res.render('project', {
+    res.render('post', {
       ...post,
       logged_in: req.session.logged_in
     });
@@ -58,7 +64,7 @@ router.get('/post/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -68,7 +74,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render('dashboard', {
       ...user,
       logged_in: true
     });
@@ -80,7 +86,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
---
+++ public/js/post.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const comment = document.querySelector('#add-comment').value.trim();
+    const postId = parseInt(window.location.pathname.split('/').pop());
+
+    if (comment) {
+        const requestBody = { contents: comment, post_id: postId };
+        console.log(requestBody); // Log request body
+
+        const response = await fetch(`/api/comments/`, {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/post/${postId}');
+        } else {
+            alert('Failed to create post');
+        }
+    }
+};
+
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newFormHandler);
+
+
